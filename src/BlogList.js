753b051c1f68c1eac7da95a55496d1c5930a67bf
aveesp <aveesp@gmail.com>
2021-02-03 15:52:36 +0530

    bloglsit updated

--- src/BlogList.js
+++ src/BlogList.js
@@ -7,7 +7,6 @@ const BlogList = ({ blogs }) => {
         <div className="blog-preview" key={blog.id} >
           <Link to={`/blog/${blog.id}`}>
             <h2>{ blog.title }</h2>
-            <p>Written by { blog.author }</p>
           </Link>
         </div>
       ))}
