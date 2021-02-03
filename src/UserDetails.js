b08899d95ce4fb98751b04d9c545e9fb7a5c1d03
aveesp <aveesp@gmail.com>
2021-02-03 15:35:23 +0530

    website added for userslist

--- src/UserDetails.js
+++ src/UserDetails.js
@@ -15,7 +15,7 @@ const UserDetails = () => {
                     <h2>{user.name}</h2>
                     <p><strong>Email:-</strong>{user.company.name}</p>
                     <p><strong>Address:-</strong> { user.address.street}, { user.address.suite}, { user.address.city}, { user.address.street} - { user.address.zipcode}</p>      
-                    <p>{ user.website }</p>
+                    <p><strong>Website:-</strong>{ user.website }</p>
                 </article>
             )}
         </div>
